39dea8e46e54aa94d65dc70fa11c58b5cf2ecaa4
MomoSong <johnmor78@gmail.com>
2017-01-02 23:17:08 +0900

    no message

---
+++ Problem7/src/Problem7.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+/**
+ * Created by johnm on 2016-12-31.
+ */
+
+/*
+Problem7
+소수를 크기 순으로 나열하면 2, 3, 5, 7, 11, 13, ... 과 같이 됩니다. 이 때 10,001번째의 소수를 구하세요.
+*/
+
+/*
+1. 소수를 구한다. (long 의 max 범위)
+2. 배열에 차례로 복사한다.
+
+1. 어레이 리스트.
+2.
+
+ */
+
+public class Problem7 {
+
+}
